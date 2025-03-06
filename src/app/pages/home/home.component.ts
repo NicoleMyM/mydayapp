c8663398c854ef6dde250218899949e236cfddb1
Maturana, Nicole <nicole.maturana@arkanosoft.com>
2025-03-06 17:00:41 -0300

    Add first commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Task} from './../../models/task.model';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+
+  //tasks = signal<Task[]>([]);
 
   constructor() { }
 
